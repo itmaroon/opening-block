001c5afac39557cd46f5f9fb95caab521de683be
itmaroon <master@itmaroon.net>
2023-06-03 11:17:51 +0900

    6-3

--- build/blocks/logo-anime/index.asset.php
+++ build/blocks/logo-anime/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0e68dc3eb122d5905e36');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2e9f11ee764e7d879871');
