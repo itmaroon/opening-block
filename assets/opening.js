da80144891ad06aefdca9d3ea68f9a5abb36a9f2
itmaroon <itmaroon@icloud.com>
2024-03-23 17:16:12 +0900

    3-23

--- assets/opening.js
+++ assets/opening.js
@@ -14,6 +14,7 @@ jQuery(function ($) {
 	function setCookie(name, value, days) {
 		var expires = "";
 		if (days) {
+			console.log(days);
 			let date = new Date();
 			date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
 			expires = "; expires=" + date.toUTCString();
